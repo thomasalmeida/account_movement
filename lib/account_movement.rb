61928241e7f5952410a68fefc46d5a6589a6c915
Thomas <thomasfa86@hotmail.com>
2020-04-06 17:04:12 -0300

    Add read_file method

--- lib/account_movement.rb
+++ lib/account_movement.rb
@@ -12,6 +12,14 @@ module AccountMovement
 
     private
 
+
+    def read_file(file_path)
+      file = File.open(file_path)
+      file_data = file.readlines.map(&:chomp)
+      file.close
+
+      file_data
+    end
     def validates_args(args)
       raise StandardError.new 'Wrong number of parameters' unless args.size == 2
     end

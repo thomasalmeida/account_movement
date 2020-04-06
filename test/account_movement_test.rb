8e5d71f87d2c3e206e2ac89c6727b7640f2163db
Thomas <thomasfa86@hotmail.com>
2020-04-06 17:11:50 -0300

    Add tests

--- test/account_movement_test.rb
+++ test/account_movement_test.rb
@@ -5,7 +5,52 @@ class AccountMovementTest < Minitest::Test
     refute_nil ::AccountMovement::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_wrong_number_of_args
+    cli = AccountMovement::CLI.new()
+    assert_equal cli.start([]), 'Wrong number of parameters'
+    assert_equal cli.start(["./test/fixtures/account.csv"]), 'Wrong number of parameters'
+    assert_equal cli.start(["./test/fixtures/account.csv", "./test/fixtures/account.csv", "./test/fixtures/transaction.csv"]), 'Wrong number of parameters'
+  end
+
+  def test_file_path_wrong_or_file_not_found
+    cli = AccountMovement::CLI.new()
+    assert_equal cli.start(["./test/fixtures/account_wrong.csv", "./test/fixtures/transaction.csv"]), "No such file or directory @ rb_sysopen - ./test/fixtures/account_wrong.csv"
+  end
+
+  def test_file_with_not_valid_data
+    cli = AccountMovement::CLI.new()
+    assert_equal cli.start(["./test/fixtures/account_not_valid.csv", "./test/fixtures/transaction.csv"]), 'Non-integer type data in files'
+    assert_equal cli.start(["./test/fixtures/account.csv", "./test/fixtures/transaction_not_valid.csv"]), 'Non-integer type data in files'
+  end
+
+  def test_duplicate_account_in_file
+    cli = AccountMovement::CLI.new()
+    assert_equal cli.start(["./test/fixtures/account_duplicate.csv", "./test/fixtures/transaction.csv"]), 'Duplicate accounts in ACCOUNT file'
+  end
+
+  def test_account_create
+    account = Account.new(id: 5, balance: 555)
+
+    assert_equal account.class, Account
+    assert_equal account.id, 5
+    assert_equal account.balance, 555
+  end
+
+  def test_transaction_to_account_not_found
+    cli = AccountMovement::CLI.new()
+    assert_equal cli.start(["./test/fixtures/account.csv", "./test/fixtures/transaction_not_found.csv"]), 'Account not found in TRANSACTION file'
+  end
+
+  def test_response
+    cli = AccountMovement::CLI.new()
+    response = cli.start(["./test/fixtures/account.csv", "./test/fixtures/transaction.csv"])
+
+    balance_final = [-10, -10, 310]
+
+    response.each_with_index do |account, index|
+      assert_equal account.class, Account
+      assert_equal account.id, index + 1
+      assert_equal account.balance, balance_final[index]
+    end
   end
 end

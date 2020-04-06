7d590898153d16b521cd75cf7e2c69b2d8a0e726
Thomas <thomasfa86@hotmail.com>
2020-04-06 16:54:55 -0300

    Add validate parameters

--- lib/account_movement.rb
+++ lib/account_movement.rb
@@ -2,5 +2,11 @@
 module AccountMovement
   class CLI
     def start(args)
+      validates_args(args)
+
+    private
+
+    def validates_args(args)
+      raise StandardError.new 'Wrong number of parameters' unless args.size == 2
     end
 end
